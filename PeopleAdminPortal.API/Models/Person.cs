219dc51e7e3a8056a97ecbe7ad344526a5f5176c
Novear Nabil <novear.nabil@softec.ai>
2022-07-19 17:10:14 +0200

    last push

--- PeopleAdminPortal.API/Models/Person.cs
+++ PeopleAdminPortal.API/Models/Person.cs
@@ -10,7 +10,7 @@ namespace PeopleAdminPortal.API.Models
         public DateTime DateOfBirth { get; set; }
         public string Email { get; set; }
         public long Mobile { get; set; }
-        public string ProfileImageUrl { get; set; }
+        public string? ProfileImageUrl { get; set; }
         public Guid GenderId { get; set; }
         public Gender Gender { get; set; }
         public Address Address { get; set; }
